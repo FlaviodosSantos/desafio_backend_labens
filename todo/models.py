96612a7ab368469797b39f43d9bc1ea5cb338718
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2024-05-17 12:53:45 -0300

    :sparkles: feat: model criado #1

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+SITUACAO = [("nova","Nova"), ("em_andamento","Em andamento"), ("concluida","Concluída"), ("cancelada","Cancelada")]
+
+class Tarefas(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=250, null=True, blank=True)
+    prazo = models.DateField()
+    data_conclusao = models.DateField()
+    situacao = models.CharField(choices=SITUACAO, max_length=13)
